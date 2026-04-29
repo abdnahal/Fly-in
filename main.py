ccaac41f49d18091889d4f46275d435110987dde
Abdellatif Nahal <abdnahal@e1r1p5.1337.ma>
2026-04-29 13:50:39 +0100

    parsing

---
+++ main.py
@@ -0,0 +1,4 @@
+from parser import ConfigParser
+
+if __name__ == "__main__":
+    parser = ConfigParser('file.txt', {})
--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 from typing import Dict, List
+import sys
 
 
 class ConfigParser():
@@ -6,14 +7,30 @@ class ConfigParser():
         self.config = path
         self.data = {}
         self.parse()
-    
+
     def parse(self) -> Dict[str, dict]:
         try:
             with open(self.config, 'r') as f:
+                self.data['hubs'] = {}
+                self.data['connections'] = {}
                 for line in f:
                     if not line.strip() or line.startswith('#'):
                         continue
                     else:
                         parts = line.split(':')
-                        if len(parts) > 2:
+                        if len(parts) != 2:
                             raise ValueError(f"Invalid format: {line}")
+                        if parts[0].lower() != 'connection':
+                            data = parts[1].split(maxsplit=3)
+                            self.data['hubs'][data[0]] = {}
+                            self.data['hubs'][data[0]]['coord'] = (int(data[1]), int(data[2]))
+                            self.data['hubs'][data[0]]['metadata'] = data[3]
+                        else:
+                            data = parts[1].split()
+                            self.data['connections'][data[0]] = {}
+                            if len(data) == 2:
+                                self.data['connections'][data[0]]['metadata'] = data[1]
+        except ValueError as e:
+            print(e)
+            sys.exit(1)
+        print(self.data['hubs'])
